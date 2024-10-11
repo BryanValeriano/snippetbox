35d5324e4f62e826ba05ceec782c22cf31ea5ec7
Bryan <bryan.b.valeriano@gmail.com>
2024-10-10 21:51:59 -0300

    add centralize error handling

---
+++ cmd/web/helpers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+	app.logger.Error(err.Error(), slog.String("method", method), slog.String("uri", uri))
+	app.clientError(w, http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
