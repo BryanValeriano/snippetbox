09026f85ff4e3f10c5046029c38a34573032cae1
Bryan <bryan.b.valeriano@gmail.com>
2024-11-03 20:58:02 -0300

    add latest snippets with template to the home page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,25 +19,25 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
 	}
 
-	//files := []string{
-	//	"./ui/html/partials/nav.tmpl",
-	//	"./ui/html/base.tmpl",
-	//	"./ui/html/pages/home.tmpl",
-	//}
+	data := templateData{
+		Snippets: snippets,
+	}
 
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//	return
-	//}
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//if err != nil {
-	//	app.serverError(w, r, err)
-	//}
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
