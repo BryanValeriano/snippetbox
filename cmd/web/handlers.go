45a701f838f0269bc5ce916ea45918afbf67077c
Bryan <bryan.b.valeriano@gmail.com>
2024-11-03 22:11:22 -0300

    add render helper function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,20 +21,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data := templateData{
 		Snippets: snippets,
 	}
-
 	page := "home.tmpl"
-
-	ts, ok := app.templateCache[page]
-	if !ok {
-		err := fmt.Errorf("the tample %s does not exist", page)
-		app.serverError(w, r, err)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	app.render(w, r, http.StatusOK, page, data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -57,20 +45,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data := templateData{
 		Snippet: snippet,
 	}
-
 	page := "view.tmpl"
-
-	ts, ok := app.templateCache[page]
-	if !ok {
-		err := fmt.Errorf("the tample %s does not exist", page)
-		app.serverError(w, r, err)
-		return
-	}
-
-	err = ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	app.render(w, r, http.StatusOK, page, data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
