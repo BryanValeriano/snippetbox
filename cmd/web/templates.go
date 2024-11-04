cf1580d10a6c3be4cdd8eb55699937f48160ff84
Bryan <bryan.b.valeriano@gmail.com>
2024-11-03 22:19:29 -0300

    add parse all partial template files to template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -22,12 +22,17 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/partials/nav.tmpl",
-			page,
+		// parse base and partial templates
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
 		}
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+		// parse page
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
